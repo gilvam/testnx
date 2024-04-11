a142770a442ec946175d910680f94553bb02c734
Gilvam Mourao <gilvamm@gmail.com>
2024-04-11 11:48:27 -0300

    first commit

--- apps/test-first/src/app/app.component.ts
+++ apps/test-first/src/app/app.component.ts
@@ -11,4 +11,16 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 })
 export class AppComponent {
   title = 'test-first';
+
+  constructor() {
+    const x: any = '';
+  }
+
+  testX(): any{
+    console.log('testX return n');
+  }
+
+  testY() {
+    console.log('testX return no especific');
+  }
 }
